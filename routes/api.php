56711d54fbeaaea91593c580f7e47d5e175c2f79
tijjvni <tijjvni@gmail.com>
2021-12-15 01:34:11 +0000

    APi login route

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\API\SpecialistsController;
 use App\Http\Controllers\API\FacilitiesController;
 use App\Http\Controllers\API\RatingsController;
