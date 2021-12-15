8709b300e4500e9efbe10e8c1c6573677886c8dc
tijjvni <tijjvni@gmail.com>
2021-12-15 03:12:37 +0000

    msge

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AccountController;
 use App\Http\Controllers\Api\AuthController;
-// use App\Http\Controllers\API\SpecialistsController;
-// use App\Http\Controllers\API\FacilitiesController;
+use App\Http\Controllers\Api\SpecialistsController;
+use App\Http\Controllers\Api\FacilitiesController;
 // use App\Http\Controllers\API\RatingsController;
 
 Route::post('/login','AuthController@login');
@@ -16,22 +16,22 @@ Route::middleware('auth:sanctum')->group(function(){
 
     Route::post('/logout','AuthController@logout');
 
-    // Route::prefix('/specialists')->group(function(){
+    Route::prefix('/specialists')->group(function(){
     
-    //     Route::GET('/', 'SpecialistsController@index');
-    //     Route::POST('/', 'SpecialistsController@create');
-    //     Route::GET('/{id}', 'SpecialistsController@show');
-    //     Route::PUT('/{id}', 'SpecialistsController@update');
-    //     Route::DELETE('/{id}', 'SpecialistsController@destroy');
+        Route::GET('/', 'SpecialistsController@index');
+        Route::POST('/', 'SpecialistsController@create');
+        Route::GET('/{id}', 'SpecialistsController@show');
+        Route::PUT('/{id}', 'SpecialistsController@update');
+        Route::DELETE('/{id}', 'SpecialistsController@destroy');
         
-    // });
+    });
     
-    // Route::prefix('/facilities')->group(function(){
+    Route::prefix('/facilities')->group(function(){
         
-    //     Route::GET('/', [FacilitiesController::class, 'index']);
-    //     Route::POST('/', [FacilitiesController::class, 'post']);
-    //     Route::GET('/{id}', [FacilitiesController::class, 'show']);
-    //     Route::PUT('/{id}', [FacilitiesController::class, 'update']);
+        Route::GET('/', [FacilitiesController::class, 'index']);
+        Route::POST('/', [FacilitiesController::class, 'post']);
+        Route::GET('/{id}', [FacilitiesController::class, 'show']);
+        Route::PUT('/{id}', [FacilitiesController::class, 'update']);
     
-    // });
+    });
 });
